78db9c95a5f61e21e7905d787fa0d4f5ab40e003
kauanfontanela <kauan.11722@alunosatc.edu.br>
2024-06-11 21:24:50 -0300

    first commit

---
+++ src/main/java/backend/medapi/MedapiApplication.java
@@ -0,0 +1,13 @@
+package backend.medapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/medapi/MedapiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.medapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
