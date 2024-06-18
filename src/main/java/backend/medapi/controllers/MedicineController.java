e7a39dc493baa740e23f3e348d9dcd1ebe92a153
kauanfontanela <kauan.11722@alunosatc.edu.br>
2024-06-17 21:26:01 -0300

    chore: Update MedicineController to use @Valid annotation for request bodies

--- src/main/java/backend/medapi/controllers/MedicineController.java
+++ src/main/java/backend/medapi/controllers/MedicineController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import backend.medapi.dtos.MedicineDto;
 import backend.medapi.dtos.MedicineDtoOpt;
 import backend.medapi.services.MedicineService;
+import jakarta.validation.Valid;
 
 @RestController
 public class MedicineController {
@@ -28,7 +29,7 @@ public class MedicineController {
     }
 
     @PostMapping("/medicines")
-    public ResponseEntity<?> add(@RequestBody MedicineDto body,
+    public ResponseEntity<?> add(@RequestBody @Valid MedicineDto body,
             @RequestParam(defaultValue = "false") Boolean handleNew) {
         try {
             medicineService.add(body, handleNew);
@@ -49,7 +50,7 @@ public class MedicineController {
     }
 
     @PatchMapping("/medicines/{name}")
-    public ResponseEntity<?> update(@PathVariable String name, @RequestBody MedicineDtoOpt body) {
+    public ResponseEntity<?> update(@PathVariable String name, @RequestBody @Valid MedicineDtoOpt body) {
         try {
             medicineService.update(name, body);
             return ResponseEntity.ok("Medicine updated successfully");
