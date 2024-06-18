dbc0ae91e7cb17b18013fd7534d67b9d3e321baa
kauanfontanela <kauan.11722@alunosatc.edu.br>
2024-06-17 21:24:39 -0300

    unused comment

--- src/main/java/backend/medapi/dtos/MedicineDto.java
+++ src/main/java/backend/medapi/dtos/MedicineDto.java
@@ -8,12 +8,4 @@ import jakarta.validation.constraints.NotEmpty;
 
 public record MedicineDto(@NotBlank String name, @NotEmpty ArrayList<String> treatsFor,
         @Nullable Boolean needsPrescription) {
-    // public MedicineDto {
-    // if (name == null || name.isBlank()) {
-    // throw new IllegalArgumentException("Name cannot be null or empty");
-    // }
-    // if (treatsFor == null || treatsFor.isEmpty()) {
-    // throw new IllegalArgumentException("Treats for cannot be null or empty");
-    // }
-    // }
 }
